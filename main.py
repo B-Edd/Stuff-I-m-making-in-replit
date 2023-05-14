d1d296f1eac30d33b7b7d93635b4e3fed30a7051
EddieWird <121364681+EddieWird@users.noreply.github.com>
2023-05-14 18:24:09 +0000

    GAME

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import hashlib
+import chess
+import chess.engine
 
-# Create a function to hash the password
-def hash_password(password):
-    salt = b'somerandomstring'  # Generate a random salt value
-    # Combine the password and salt, and hash them using SHA256
-    hashed_password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000) # salt can change
-    return hashed_password.hex()
+engine = chess.engine.SimpleEngine.popen_uci(
+  "stockfish_15.1_win_x64_popcnt/stockfish-windows-2022-x86-64-modern.exe"
+)
 
-# Get the user's password
-password = input("Enter your password: ")
+board = chess.Board()
 
-# Hash the password and store it in a file
-with open('password.txt', 'a') as f:
-    f.write('\n')
-    f.write(hash_password(password))
+result = engine.play(board, chess.engine.Limit(time=2.0))
+best_move = result.move
+
+board.push(best_move)
+
+engine.quit()
